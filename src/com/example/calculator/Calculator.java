8ea8f57b6bd3bff8751756e82226acad871a2f60
sooyeon_heo <suzi97@naver.com>
2024-10-17 12:03:51 +0900

    계산기 v.2 -> 결과값 반환 메서드 구현 & 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator.java 클래스 생성

---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.example.calculator;
+// 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
+public class Calculator {
+    public int calculate(int firstNumber, int secondNumber, char operator) {
+
+    }
+}
