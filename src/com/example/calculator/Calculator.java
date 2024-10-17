a7c8c7448c667dccf8a6c5aca98844475cda4df5
sooyeon_heo <suzi97@naver.com>
2024-10-17 12:09:00 +0900

    계산기 v.2.1 -> 사칙 연산 기능 추가

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -2,6 +2,22 @@ package com.example.calculator;
 // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
 public class Calculator {
     public int calculate(int firstNumber, int secondNumber, char operator) {
+        int result = 0;
 
+        // 사칙 연산을 수행
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                result = firstNumber / secondNumber;
+                break;
+        }
     }
 }
