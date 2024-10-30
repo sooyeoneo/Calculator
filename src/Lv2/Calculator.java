aee97b6409284552ba66328c5bce08d765a3a506
sooyeon_heo <suzi97@naver.com>
2024-10-30 10:44:38 +0900

    계산기 v.2.8 -> [캡슐화] App 클래스의 main 메서드에 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근 못하도록 수정 (간접 접근으로 가져오기 Getter, 간접 접근으로 수정 Setter)

--- src/Lv2/Calculator.java
+++ src/Lv2/Calculator.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Calculator {
+    // 필드 캡슐화 - 컬렉션 타입 필드를 private 선언
     // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
     private List<Double> results = new ArrayList<>();
 
@@ -32,13 +33,18 @@ public class Calculator {
             default:
                 throw new IllegalArgumentException("올바른 사칙연산 기호가 아닙니다.");
         }
-
+        // 결과를 리스트에 저장
+        results.add(result);
         return result;
     }
-    // 모든 연산 결과를 반환하는 메서드
+    // 모든 연산 결과를 반환하는 메서드 Getter, 외부에서 연산 결과를 조회할 수 있음.
     public List<Double> getResults() {
-        return results;
+        return new ArrayList<>(results); // 캡슐화 유지를 위해 새 리스트 반환, 원본 results 리스트 보호
     }
 
+    // results 필드를 설정하는 Setter
+    public void setResults(List<Double> results) {
+        this.results = results;
+    }
 }
 
