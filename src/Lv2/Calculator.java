172b62eb62415650df19ec4ae60c8d1c6ee0b5ff
sooyeon_heo <suzi97@naver.com>
2024-10-30 10:15:59 +0900

    계산기 v.2.7 -> Calculator 클래스에 사칙연산을 수행 후 결과값 반환 메서드 구현 & 연산 결과를 저장하는 컬렉션 타입 필드 생성

--- src/Lv2/Calculator.java
+++ src/Lv2/Calculator.java
@@ -1,4 +1,45 @@
 package Lv2;
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Calculator {
+    // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
+    private List<Double> results = new ArrayList<>();
+
+    // 사칙 연산을 수행하고 결과를 반환하는 메서드
+    public double calculate(double firstNumber, double secondNumber, char operator) {
+        double result = 0;
+
+        // 사칙 연산을 수행
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                if (secondNumber != 0) {
+                    result = firstNumber / secondNumber;
+                } else {
+                    throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("올바른 사칙연산 기호가 아닙니다.");
+        }
+
+        return result;
+    }
+    // 모든 연산 결과를 반환하는 메서드
+    public List<Double> getResults() {
+        return results;
+    }
+
 }
+
